9c527097bfd91a0f2f261c05e32ee8a32a494f8a
Peter Lu <peterlu94@gmail.com>
2024-10-20 19:30:24 -0400

    Added css for data-section, added classes to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useCallback, useMemo, useState } from "react";
 import "./App.css";
 import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from "./consts";
-import Attribute from "./components/Attribute";
+import Attribute from "./components/Attribute/Attribute";
+import CharClass from "./components/CharClass/CharClass";
 
 function App() {
   const initialAttributes = useMemo(
@@ -39,7 +40,7 @@ function App() {
         <h1>React Coding Exercise</h1>
       </header>
       <section className="App-section">
-        <section className="attributes-section">
+        <section className="data-section">
           <h2>Attributes</h2>
           <h3>Total Attributes Allocated: {totalAttributePoints}</h3>
           {ATTRIBUTE_LIST.map((attribute) => (
@@ -48,6 +49,18 @@ function App() {
               attributeName={attribute}
               currentTotalAttributes={totalAttributePoints}
               onAttributeChange={handleAttributeUpdate}
+            />
+          ))}
+        </section>
+
+        <section className="data-section">
+          <h2>Classes</h2>
+          {Object.entries(CLASS_LIST).map(([className, classAttributes]) => (
+            <CharClass
+              key={className}
+              charClassName={className}
+              charClassAttributes={classAttributes}
+              currentAttributes={currentAttributes}
             />
           ))}
         </section>
